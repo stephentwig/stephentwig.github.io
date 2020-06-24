18bd9562fe680fa91301e89738b3bf2fbc2275a3
stephentwig <step.twig@gmail.com>
2020-06-24 14:20:08 +0100

    converted cart to an array

--- vueApp/main.js
+++ vueApp/main.js
@@ -99,7 +99,7 @@ Vue.component('product', {
          
         methods: {
             addToCart: function () {
-                this.$emit('add-to-cart')
+                this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
             },
 
             updateProduct: function (index) {
@@ -134,12 +134,12 @@ var app = new Vue ({
     el: '#app',
     data: {
         premium: false,
-        cart: 0
+        cart: []
     },
 
     methods: {
-        updateCart() {
-            this.cart += 1
+        updateCart(id) {
+            this.cart.push(id)
         }
     }
 
