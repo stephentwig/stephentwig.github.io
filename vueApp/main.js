e16e5f085c58c0ce7280407e779537cd3fb04df5
stephentwig <step.twig@gmail.com>
2020-06-24 13:26:19 +0100

    converted to product components

--- vueApp/main.js
+++ vueApp/main.js
@@ -1,32 +1,80 @@
-var app = new Vue ({
-    el: '#app',
-    data: {
-        brand: 'Vue Mastery',
-        product: 'Socks',
-        selectedVariant: 0,
-        url: 'https://vuejs.org',
-        inventory: 8,
+Vue.component('product', {
+    template: `
         
-        details: ["80% cotton","20% polyester","Gender-neutral"],
-        variants: [
-            {  
-                variantId: 2234 ,
-                variantColor: "green",
-                variantImage: 'images/vmSocks-green-onWhite.jpg',
-                variantQuantity: 0
+    <div id="product" class="product">
 
-            },
-            {  
-                variantId: 2235 ,
-                variantColor: "blue",
-                variantImage: 'images/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 10
+    <div class="product-image">
+        <img v-bind:src="image">
+    </div>
 
-            }
-        ],
-        cart: 0
-        
-         },
+    <div class="product-info">
+        <h1> <a :href="url">{{ title }}</a> </h1>
+
+        <!-- if and else statements -->
+        <p v-if="inStock > 10">In Stock</p>
+        <p v-else-if="inStock <= 10 && inStock > 0">Almost Out of Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <!-- show /hide  same as style= "display:none" -->
+        <!-- <p v-show="inStock">Promotion</p> -->
+
+        <ul>
+            <li v-for="detail in details"> {{ detail }} </li>
+        </ul>
+
+        <!-- for multiple items in list or array -->
+        <div v-for="(variant, index) in variants" 
+            :key="variant.variantId"
+            class="color-box"
+            :style="{ backgroundColor: variant.variantColor }"
+            @mouseover="updateProduct(index)">
+        </div>
+        <!-- v-on or the @ sign -->
+        <button v-on:click="addToCart"
+                :disabled="!inStock"
+                :class="{ disabledButton: !inStock }">Add to Cart </button>
+
+        <div class="cart">
+            <p> Cart ({{ cart }})</p>
+
+        </div>
+
+
+    </div>
+
+    <!-- <h1>{{ product + " is clean" }}</h1> -->
+</div>
+
+    `,
+    data() {
+        return {
+            brand: 'Vue Mastery',
+            product: 'Socks',
+            selectedVariant: 0,
+            url: 'https://vuejs.org',
+            inventory: 8,
+            
+            details: ["80% cotton","20% polyester","Gender-neutral"],
+            variants: [
+                {  
+                    variantId: 2234 ,
+                    variantColor: "green",
+                    variantImage: 'images/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 0
+    
+                },
+                {  
+                    variantId: 2235 ,
+                    variantColor: "blue",
+                    variantImage: 'images/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 10
+    
+                }
+            ],
+            cart: 0
+            
+             }
+    },
          
         methods: {
             addToCart: function () {
@@ -51,4 +99,10 @@ var app = new Vue ({
         }
 
     }
+
+})
+
+var app = new Vue ({
+    el: '#app'
+
 })
